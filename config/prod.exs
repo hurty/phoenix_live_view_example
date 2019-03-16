9db9b67dd9ecdb7a6e9120714a360683948cadcd
Pierre Hurtevent <pierre.hurtevent@gmail.com>
2019-03-16 17:44:04 +0100

    change prod host name

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :demo, DemoWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "liveview.cleverapps.io", port: 443],
+  force_ssl: [hsts: true, rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
